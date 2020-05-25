d346841657841e487dff695248638a497bf8fcdb
Edson Caliman <edsonjunior@db1.com.br>
2020-05-25 09:20:56 -0300

    Versão inicial

---
+++ APICatalogo/APICatalogo/Migrations/20200522120915_Populadb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace APICatalogo.Migrations
+{
+    public partial class Populadb : Migration
+    {
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Bebidas', 'http://macoratti.net/imagens/1.jpg')");
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Lanches', 'http://macoratti.net/imagens/2.jpg')");
+            mb.Sql("insert into Categorias(Nome, ImagemUrl) values ('Sobremesas', 'http://macoratti.net/imagens/3.jpg')");
+
+
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) values " +
+                "('Coca-Cola Diet', 'Refrigerante de cola', '5.45', 'http://macoratti.net/imagens/coca.jpg', 50, now(), " +
+                "(select CategoriaId from Categorias where Nome='Bebidas'))");
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) values " +
+                "('Lanche de Atum', 'Lanche de Atum com maionese', '8.50', 'http://macoratti.net/imagens/atum.jpg', 10, now(), " +
+                "(select CategoriaId from Categorias where Nome='Lanches'))");
+            mb.Sql("insert into Produtos(Nome, Descricao, Preco, ImagemUrl, Estoque, DataCadastro, CategoriaId) values " +
+                "('Pudim 100 g', 'Pudim de leite condensado 100g', '6.75', 'http://macoratti.net/imagens/pudim.jpg', 20, now(), " +
+                "(select CategoriaId from Categorias where Nome='Sobremesas'))");
+        }
+
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from produtos");
+            mb.Sql("delete from categorias");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+Aplicação desenvolvida a partir do curso do Macoratti na Udemy.
+
+https://www.udemy.com/course/curso-web-api-asp-net-core-essencial
+
+
+Tipo de Projeto: .Net Core 3.1 Web Api Console Application
+
+Comandos migrations:
+
+add-migration <nome-migration> - cria a migration
+update-database - atualiza o banco de dados com as migrations criadas
+update-database -migration <nome-migration> - executa uma migration específica
