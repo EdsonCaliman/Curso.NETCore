3cdee2ca3f0c0e861bc14903eeef8c2e80152d97
Edson Caliman <edsonjunior@db1.com.br>
2020-06-08 09:09:27 -0300

    ValidationAttribute e ValidatableObject

--- APICatalogo/APICatalogo/Models/Produto.cs
+++ APICatalogo/APICatalogo/Models/Produto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using APICatalogo.Validations;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -8,17 +9,19 @@ using System.Threading.Tasks;
 namespace APICatalogo.Models
 {
     [Table("Produtos")]
-    public class Produto
+    public class Produto : IValidatableObject
     {
         [Key]
         public int ProdutoId { get; set; }
-        [Required]
+        [Required(ErrorMessage ="O nome é obrigatório")]
         [MaxLength(80)]
+        [PrimeiraLetraMaiusculaAttribute]
         public string Nome { get; set; }
-        [Required]
+        [Required(ErrorMessage = "A descrição é obrigatória")]
         [MaxLength(300)]
         public string Descricao { get; set; }
-        [Required]
+        [Required(ErrorMessage = "O preço é obrigatório")]
+        [Range(1, 10000, ErrorMessage ="O preço deve estar entre {1} e {2}")]
         public decimal Preco { get; set; }
         [Required]
         [MaxLength(500)]
@@ -27,5 +30,22 @@ namespace APICatalogo.Models
         public DateTime DataCadastro { get; set; }
         public Categoria Categoria { get; set; }
         public int CategoriaId { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (!string.IsNullOrEmpty(this.Nome))
+            {
+                var primeiraLetra = this.Nome[0].ToString();
+                if (primeiraLetra != primeiraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("A primeira letra do produto deve ser maiúscula.",
+                    new[]
+                    {
+                        nameof(this.Nome)
+                    }
+                    );
+                }
+            }
+        }
     }
 }
